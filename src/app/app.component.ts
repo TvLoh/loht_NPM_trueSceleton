96f7eb92abe4faec90774db11f0e85f229754f70
Thomas von Loh <Thomas.von-Loh@adesso.de>
2023-06-27 19:29:08 +0200

    remove show example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,4 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  public items: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-
-  clickEvent() {
-    console.log('click click')
-  }
 }
